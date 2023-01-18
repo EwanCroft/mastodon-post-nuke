3aa6d5f1a6e6b9cd743edb2f093ac6876b053842
Ewan Croft <121812160+EwanCroft@users.noreply.github.com>
2023-01-18 14:24:54 +0000

    Update nuke.py

--- nuke.py
+++ nuke.py
@@ -2,7 +2,9 @@ import configparser
 import os
 from mastodon import Mastodon
 
-if not os.path.exists('config.ini'):
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if not os.path.exists(rf'{ROOT_DIR}\config.ini'):
     url = input("Enter the URL of your Mastodon instance:\n")
     email = input("Enter your email address:\n")
     password = input("Enter your password:\n")
@@ -11,23 +13,33 @@ if not os.path.exists('config.ini'):
         "Post Nuke",
         api_base_url = f"{url}"
     )
+    client_id, client_secret = app_info
+
+    mastodon = Mastodon(client_id=client_id, client_secret=client_secret, api_base_url=url)
+    access_token = mastodon.log_in(email, password)
 
     config = configparser.ConfigParser()
-    config['MASTODON'] = {'url': url, 'email': email, 'password': password, 'client_id': app_info.client_id, 'client_secret': app_info.client_secret}
+    config['MASTODON'] = {'url': url,
+                          'email': email,
+                          'password': password,
+                          'client_id': client_id,
+                          'client_secret': client_secret,
+                          'access_token': access_token}
+    
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(rf'{ROOT_DIR}\config.ini')
 url = config['MASTODON']['url']
 email = config['MASTODON']['email']
 password = config['MASTODON']['password']
 client_id_str = config['MASTODON']['client_id']
 client_secret_str = config['MASTODON']['client_secret']
+access_token_str = config['MASTODON']['access_token']
 
-mastodon = Mastodon(client_id = client_id_str)
+mastodon = Mastodon(client_id=client_id_str, client_secret=client_secret_str, access_token=access_token_str, api_base_url=url)
 
-mastodon.log_in(email, password)
 
 max_id = None
 while True:
@@ -38,3 +50,5 @@ while True:
         mastodon.status_delete(post['id'])
         print(f"Post {post['id']} deleted.")
     max_id = min([p['id'] for p in posts]) - 1
+
+print("Post history deleted.")
